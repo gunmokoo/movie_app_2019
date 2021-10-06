5e94e86c421682131976adc59cee5c112e0a8990
ging <minju1348@gmail.com>
2021-10-07 01:14:50 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-//import logo from './logo.svg';
-//import "./App.css";
-import Potato from "./potato";
+import React from "react";
+
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgeopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
